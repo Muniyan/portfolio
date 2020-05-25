0c027c0b05d72bffb06a42d6450d242cde4dd635
Muniyan <50628045+Muniyan@users.noreply.github.com>
2020-05-25 20:19:02 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -9,10 +9,21 @@ import Resume from './component/resume';
 import Blog from './component/blog';
 
 class App extends Component {
-  render() {
+
+  renderRedirect = () => {
     console.log("App========================== ", window.location);
+    let search = window.location.search;
+    if(search.indexOf("?p=") != 0) {
+      let url = window.location.pathname + window.location.search.slice(3);
+      console.log("App url========================== ", url);
+      return <Redirect to={url} />
+    }
+  }
+
+  render() {
     return (
       <div className="container">
+        { this.renderRedirect() }
         <div className="l-p1">
           <Sidebar />
         </div>
