b48af30a3a5bc871314c66bbe0b7f3fa98570741
Muniyan <50628045+Muniyan@users.noreply.github.com>
2020-06-22 18:13:41 +0530

    Updates

---
+++ precache-manifest.559c907781566723c73e7b52a2de0bdf.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28d1fd0079c04be215f24a5e4126501e",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "f4495949f0f8ba23dc17",
+    "url": "/portfolio/static/css/main.578fe9c1.chunk.css"
+  },
+  {
+    "revision": "5b8bfec6409f0afd592a",
+    "url": "/portfolio/static/js/2.06784ee8.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/portfolio/static/js/2.06784ee8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f4495949f0f8ba23dc17",
+    "url": "/portfolio/static/js/main.12a80d52.chunk.js"
+  },
+  {
+    "revision": "4bd91fadd2fd7eb0d6b8",
+    "url": "/portfolio/static/js/runtime-main.32e6bbb7.js"
+  },
+  {
+    "revision": "272113c6ea8035ada5864bb5a84a1786",
+    "url": "/portfolio/static/media/muniyan.272113c6.jpg"
+  },
+  {
+    "revision": "6ca318aeefa587877f1bcb7e854407c9",
+    "url": "/portfolio/static/media/muniyan.6ca318ae.pdf"
+  }
+]);
